fa468acd7360987b86c2c2019272684f967f45f6
Pavel Yvorskiy <pavelyvorski@icloud.com>
2023-02-15 19:37:54 +0200

    master

--- assets/script/swiper.js
+++ assets/script/swiper.js
@@ -13,36 +13,31 @@ const swiper = new Swiper(".mySwiper", {
   },
   });
 
-  const swiper2 = new Swiper(".mySwiper2", {
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 30,
     slidesPerView: 2,
-    spaceBetween: 10,
-    autoHeight: true,
-    breakpoints: {
-      480: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 30
-      },
-      1000: {
-        slidesPerView: 5,
-        spaceBetween: 40
-      }
-    },
-    loop: true,
-    speed:2000,
+    centeredSlides: true,
+    loop:true,
     autoplay: {
-      delay: 5000,
+      delay: 2500,
       disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: false,
-  },
+    breakpoints: {
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
   });
