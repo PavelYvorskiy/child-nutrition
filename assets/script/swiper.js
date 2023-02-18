417ccd1d6387e02d3205d229a9caf6904534985c
Pavel Yvorskiy <pavelyvorski@icloud.com>
2023-02-18 19:58:42 +0200

    master

--- assets/script/swiper.js
+++ assets/script/swiper.js
@@ -1,25 +1,24 @@
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    centeredSlides: true,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: false,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: false,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40
-    },
-  }
-  });
+  pagination: {
+  el: ".swiper-pagination",
+  dynamicBullets: false,
+},
+breakpoints: {
+  1000: {
+    slidesPerView: 2,
+  },
+}
+});
 
-  var swiper2 = new Swiper(".mySwiper2", {
+  const swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 30,
     slidesPerView: 2,
     speed:3000,
